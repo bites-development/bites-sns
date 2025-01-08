ca7e9956a187943219444bd1396a0e88ad75b322
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2025-01-08 16:01:53 +0200

    Force add Sns

--- src/Resources/config/config.php
+++ src/Resources/config/config.php
@@ -3,9 +3,9 @@
 use Modules\Sns\Events\SnsMessageReceived;
 
 return [
-    'key' => env('AWS_ACCESS_KEY_ID'),
-    'secret' => env('AWS_SECRET_ACCESS_KEY'),
-    'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    'key' => env('SNS_ACCESS_KEY_ID'),
+    'secret' => env('SNS_SECRET_ACCESS_KEY'),
+    'region' => env('SNS_DEFAULT_REGION', 'us-east-1'),
     'listeners' => [
         SnsMessageReceived::class => [
 
